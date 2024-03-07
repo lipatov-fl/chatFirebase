7cf4865b91cee7edf256f00ce418fd50f08d7811
Alexandr Lipatov <lipatov-fl@yandex.ru>
2024-03-07 19:14:34 +0300

    Завершил Authentication по Google аккаунту

--- app/src/main/java/com/example/fbchat/SignInAct.kt
+++ app/src/main/java/com/example/fbchat/SignInAct.kt
@@ -1,25 +1,50 @@
 package com.example.fbchat
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import com.example.fbchat.databinding.ActivityMainBinding
+import com.example.fbchat.databinding.ActivitySignInBinding
 import com.google.android.gms.auth.api.identity.BeginSignInRequest
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.Api
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.auth
 
 class SignInAct : AppCompatActivity() {
-    lateinit var launcher: ActivityResultLauncher<Intent>
+    private lateinit var launcher: ActivityResultLauncher<Intent>
+    private lateinit var auth: FirebaseAuth // Класс для авторизации
+    private lateinit var binding: ActivitySignInBinding
+
+    @SuppressLint("SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        auth = Firebase.auth
         launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-        // наш callback, который выдает результат
-
+            // наш callback, который выдает результат
+            val task = GoogleSignIn.getSignedInAccountFromIntent(it.data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                if (account !== null) {
+                    firebaseAuthWithGoogle(account.idToken!!)
+                }
+            } catch (e: ApiException) {
+                Log.d("MyLog", "ApiException")
+            }
+        }
+        binding.btnSignIn.setOnClickListener {
+            signInWithGoogle()
         }
     }
 
@@ -38,5 +63,12 @@ class SignInAct : AppCompatActivity() {
 
     private fun firebaseAuthWithGoogle(idToken: String) {  // Подключаем наш аккаунт
         val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential).addOnCompleteListener {
+            if (it.isSuccessful) {
+                Log.d("MyLog", "Google signIn done")
+            } else {
+                Log.d("MyLog", "Google signIn error")
+            }
+        }
     }
 }
